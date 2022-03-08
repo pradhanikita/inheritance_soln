5c40c6053ed7f390e0b03f1d47615193fa8dff20
dell <dell@DESKTOP-L36T5DT>
2022-03-08 10:34:21 +0545

    Add project files.

---
+++ inheritance_soln/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace inheritance_soln
+{
+    class Person
+    {
+       public string name;
+       public void task()
+        {
+        Console.WriteLine("Hello World!");
+
+        }
+}
+    class Nikita:Person
+    {
+        public void getname()
+        {
+            Console.WriteLine("My name is " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nikita nikita = new Nikita();
+            nikita.name = "Nikita";
+            nikita.task();
+
+            nikita.getname();
+
+            Console.ReadLine();
+        }
+    }
+}
